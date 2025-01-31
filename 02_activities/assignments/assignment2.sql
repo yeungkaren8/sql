ed13b374ec0ccd2718a35dcf807cf087896575d3
yeungkaren8 <ka.yeung@mail.utoronto.ca>
2025-01-31 14:17:37 -0500

    Completed assignment2.sql section 3.

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -144,7 +144,21 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
-
+SELECT vendor_name, product_name
+, SUM(sales_per_item) AS sales
+FROM( -- create sales per item per vendor x number of customers table
+	SELECT DISTINCT vendor_name, product_name, original_price
+	, original_price*5 AS sales_per_item, customer_id
+	FROM vendor_inventory vi
+	JOIN vendor v
+		ON vi.vendor_id = v.vendor_id
+	JOIN product p
+		on vi.product_id = p.product_id
+	CROSS JOIN 
+	(SELECT DISTINCT customer_id
+	FROM customer)
+)
+GROUP BY vendor_name, product_name
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -152,11 +166,19 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS product_units; --to make sure this temp table doesn't already exist
+CREATE TEMP TABLE product_units AS
+	SELECT *
+	, CURRENT_TIMESTAMP AS snapshot_timestamp
+	FROM product
+	WHERE product_qty_type = 'unit';
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
+INSERT INTO product_units
+VALUES(11, 'Apple Pie - small', '8"', 3, 'unit', CURRENT_TIMESTAMP);
 
 
 -- DELETE
@@ -164,6 +186,9 @@ This can be any product you desire (e.g. add another record for Apple Pie). */
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
+DELETE FROM product_units
+--SELECT * FROM product_units -- check that the right record is selected for deletion
+WHERE product_id = 11;
 
 
 -- UPDATE
@@ -183,6 +208,30 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
+ALTER TABLE product_units
+ADD current_quantity INT;
+
+--find last quantity per product based on vendor_inventory
+DROP TABLE IF EXISTS last_quantity_inventory; --to make sure this temp table doesn't already exist
+CREATE TEMP TABLE last_quantity_inventory AS
+	SELECT last.product_id
+	, COALESCE(last.quantity, 0) AS current_quantity --set NULL to 0 (i.e. product was never stocked in vendor_inventory)
+	FROM( -- for every product_id, order by most current market_date
+		SELECT *
+		, DENSE_RANK() OVER (PARTITION BY vi.product_id ORDER BY vi.market_date DESC) as most_current
+		FROM product p
+		LEFT JOIN vendor_inventory vi -- LEFT JOIN so that all products (in product) has corresponding info from vendor_inventory (otherwise, values are NULL but master product info is still there)
+			ON p.product_id = vi.product_id 
+	) AS last
+	WHERE most_current = 1;
+
+
+UPDATE product_units
+SET current_quantity = (
+	SELECT current_quantity
+	FROM last_quantity_inventory
+	WHERE product_units.product_id = last_quantity_inventory.product_id -- WHERE statement has to be inside the SET, since calling last_quantity_inventory table inside this subquery
+)
 
 
 
